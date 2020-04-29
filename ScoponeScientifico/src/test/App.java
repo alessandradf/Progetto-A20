28c7c904cffd78ef26c359544b982c5a08d47151
stefa <stefa@192.168.1.5>
2020-04-29 18:29:49 +0200

    pushed 1337 message prova

--- ScoponeScientifico/src/test/App.java
+++ ScoponeScientifico/src/test/App.java
@@ -3,5 +3,6 @@ package test;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
+        System.out.println("1337");
     }
 }
