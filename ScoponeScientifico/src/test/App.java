12a679c87bffecebf78cb5d51634455403deae9f
angii <angii@LAPTOP-ENCE5R3V.station>
2020-04-29 20:05:44 +0200

    # WARNING: head commit changed in the meantime
    primo commit Angelica

--- ScoponeScientifico/src/test/App.java
+++ ScoponeScientifico/src/test/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
         System.out.println("1337");
+        System.out.println("ciao");
     }
 }
