3f7a99d7f7cae1b20b0c41abe8e4339aa74cab6f
Andrea <30377977+TreCani@users.noreply.github.com>
2020-05-17 16:30:19 +0200

    Update Team.java

--- ScoponeScientifico/src/model/Team.java
+++ ScoponeScientifico/src/model/Team.java
@@ -1,10 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+
 /**
  * Rappresenta le due squadre di scopone
+ * 
  * @author Andrea
  *
  */
 public class Team {
 
+    private String teamName;
+    private ArrayList<Player> playersInTeam;
+
+    public Team(String name) {
+        this.teamName = name;
+    }
 }
