5e1417d7425427fa54a24a232f2f026e8e962aa9
Stefano Albini <62016694+stefano-albini@users.noreply.github.com>
2020-05-17 18:36:27 +0200

    Aggiunta possibilità di aggiungere un player al Team

--- ScoponeScientifico/src/model/Team.java
+++ ScoponeScientifico/src/model/Team.java
@@ -16,4 +16,16 @@ public class Team {
     public Team(String name) {
         this.teamName = name;
     }
+    
+    /*
+     * Aggiunge un Player al Team.
+     * Controlla prima se è già stato aggiunto
+     */
+    public boolean addPlayer(Player p) {
+    	if(!this.playersInTeam.contains(p)) {
+    		this.playersInTeam.add(p);
+    		return true;
+    	}
+    	return false;
+    }	
 }
