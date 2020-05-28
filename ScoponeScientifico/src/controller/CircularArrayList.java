8dfc82e1c27419384815e8f5fca11a75f004de3c
Andrea <30377977+TreCani@users.noreply.github.com>
2020-05-28 20:28:13 +0200

    Corretto un typo e visibilità di un metodo
    setCURRENT_INDEX deve essere privato. Altrimenti può essere modificato da ovunque

--- ScoponeScientifico/src/controller/CircularArrayList.java
+++ ScoponeScientifico/src/controller/CircularArrayList.java
@@ -1,18 +1,19 @@
 package controller;
 
 import java.util.ArrayList;
+import model.Game;
 
 public class CircularArrayList<E> extends ArrayList<E> {
 	private static int CURRENT_INDEX = 0;
 
 	public E getNext() {
 		CURRENT_INDEX++;
-		if (CURRENT_INDEX == 4)
+		if (CURRENT_INDEX == Game.PLAYER_NUMBER)	//Game.PLAYER_NUMBER = 4;
 			setCURRENT_INDEX(0);
 		return get(CURRENT_INDEX);
 	}
 	
-	public static void setCURRENT_INDEX(int cURRENT_INDEX) {
-		CURRENT_INDEX = cURRENT_INDEX;
+	private static void setCURRENT_INDEX(int newIndex) {
+		CURRENT_INDEX = newIndex;
 	}
 }
