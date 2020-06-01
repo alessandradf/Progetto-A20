fce7f3d3638d6ea1895662897ab3de2c382103b3
Stefano Albini <62016694+stefano-albini@users.noreply.github.com>
2020-06-01 11:45:39 +0200

    Aggiunta implementazione ComputerPlayerHandler

--- ScoponeScientifico/src/controller/AbstractPlayerHandler.java
+++ ScoponeScientifico/src/controller/AbstractPlayerHandler.java
@@ -3,18 +3,25 @@ package controller;
 import model.Player;
 
 public abstract class AbstractPlayerHandler {
-	private Player player;
+	
+	private GameController controller;	//usato per la comunicazione della carta giocata
+	private Player player;	
 	/*
 	 * private InterfaceCard cardPlayed;
 	 */
 	
-	public AbstractPlayerHandler(Player player) {
+	public AbstractPlayerHandler(Player player, GameController controller) {
 		this.player = player;
+		this.controller = controller;
 	}
 	
 	public abstract boolean unlockPlayer();
 
 	public Player getPlayer() {
 		return player;
+	}
+	
+	public GameController getController() {
+		return this.controller;
 	}
 }
--- ScoponeScientifico/src/controller/ComputerPlayerHandler.java
+++ ScoponeScientifico/src/controller/ComputerPlayerHandler.java
@@ -1,22 +1,57 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import model.Card;
 import model.Player;
+import model.SeedType;
 
 public class ComputerPlayerHandler extends AbstractPlayerHandler {
+	
+	private Random randomGenerator;
 
-	public ComputerPlayerHandler(Player player) {
-		super(player);
-		
+	public ComputerPlayerHandler(Player player, GameController controller) {
+		super(player, controller);
+		this.randomGenerator = new Random();
 	}
 
 	@Override
 	public boolean unlockPlayer() {
-		
-		return false;
+		Card c = this.pickACard();
+		//this.getController().playCard(this, c);
+		return true;
 	}
 	
-	public void pickACard() {
-		
+	/*
+	 * Ritorna una carta casuale dall'ArrayList del Player del modello al quale è associato
+	 */
+	public Card pickACard() {
+		int handSize = this.getPlayer().getHand().size();
+		int randIndex = this.randomGenerator.nextInt(handSize);
+		return this.getPlayer().getHand().get(randIndex);
 	}
 
+	
+	//La uso per testare, poi la tolgo
+	/*
+	public static void main(String[] args) {
+		
+		Player p = new Player("Debug");
+		ComputerPlayerHandler ph = new ComputerPlayerHandler(p, new GameController());
+		
+		ArrayList<Card> cards = new ArrayList<Card>();
+		
+		cards.add(new Card(3, SeedType.CUORI));
+		cards.add(new Card(5, SeedType.FIORI));
+		cards.add(new Card(5, SeedType.CUORI));
+		cards.add(new Card(7, SeedType.PICCHE));
+		
+		p.setHand(cards);
+		
+		System.out.println(p);
+		System.out.println(ph.pickACard());	
+	}
+	*/
+	
 }
