0d1af6cec21a8d144dccf2d9045cf2464dedf1ac
angelica-seiberl <angelica.seiberl01@universitadipavia.it>
2020-06-04 18:36:17 +0200

    Override equals CardLabel

--- ScoponeScientifico/src/CardTest/CardLabel.java
+++ ScoponeScientifico/src/CardTest/CardLabel.java
@@ -84,7 +84,18 @@ public class CardLabel extends JLabel {
 			}
 		}
 	}
-
+	
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		if(((CardLabel)obj).getSeed() == this.seed && (((CardLabel)obj).getValue() == this.value )){
+			
+			return true;
+			
+		}
+		return false;
+	}
+	
 	public CardPosition getPosition() {
 		return this.position;
 	}
@@ -96,5 +107,7 @@ public class CardLabel extends JLabel {
 	public int getValue() {
 		return value;
 	}
+
+	
 
 }
