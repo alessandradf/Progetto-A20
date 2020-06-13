2010b93e5b8241a48c8c773da995e06a09b07b8e
alessandradf <62052270+alessandradf@users.noreply.github.com>
2020-06-13 09:44:19 +0200

    tolt stampe di debug da GameProcessor

--- ScoponeScientifico/src/utility/GameProcessor.java
+++ ScoponeScientifico/src/utility/GameProcessor.java
@@ -20,16 +20,16 @@ public class GameProcessor {
 		int numberOfCard = onTable.size();
 		if (numberOfCard == 0)
 			return null;
-		
+
 		ArrayList<ArrayList<Card>> resultCards = new ArrayList<ArrayList<Card>>();
 		ArrayList<Card> doubleCard = searchDouble(onTable, playedCard);
-		
+
 		if (doubleCard != null) {
 			resultCards.add(doubleCard);
 			return resultCards;
 		}
 
-		else {			
+		else {
 			ArrayList<Integer> numbers = new ArrayList<Integer>();
 			for (Card c : onTable) {
 				numbers.add(c.getValue());
@@ -37,7 +37,7 @@ public class GameProcessor {
 			int target = playedCard.getValue();
 			ArrayList<ArrayList<Integer>> resultNumbers = new ArrayList<>();
 			sum_up(numbers, target, resultNumbers);
-			
+
 			if (resultNumbers.size() == 0)
 				return null;
 
@@ -58,10 +58,10 @@ public class GameProcessor {
 		}
 
 	}
-	
-	 static void sum_up(ArrayList<Integer> numbers, int target, ArrayList<ArrayList<Integer>> result) {
-	        sum_up_recursive(numbers,target,new ArrayList<Integer>(), result);
-	    }
+
+	static void sum_up(ArrayList<Integer> numbers, int target, ArrayList<ArrayList<Integer>> result) {
+		sum_up_recursive(numbers, target, new ArrayList<Integer>(), result);
+	}
 
 	static void sum_up_recursive(ArrayList<Integer> numbers, int target, ArrayList<Integer> partial,
 			ArrayList<ArrayList<Integer>> result) {
@@ -84,35 +84,16 @@ public class GameProcessor {
 			sum_up_recursive(remaining, target, partial_rec, result);
 		}
 	}
-	
+
 	private static ArrayList<Card> searchDouble(List<Card> onTable, Card playedCard) {
 		ArrayList<Card> trick = new ArrayList<Card>();
 		for (Card c : onTable) {
 			if ((c.getValue() == playedCard.getValue()) && (c.getSeed() != playedCard.getSeed())) {
-				trick.add(c);				
+				trick.add(c);
 				return trick;
 			}
 		}
 		return null;
-	}
-
-	public static void main(String[] args) {
-		ArrayList<Card> onTable = new ArrayList<Card>();
-		Card due = new Card(2, SeedType.CUORI);
-		Card t = new Card(3, SeedType.CUORI);
-		Card c = new Card(5, SeedType.CUORI);
-		Card dieci = new Card(10, SeedType.CUORI);
-		Card o = new Card(8, SeedType.CUORI);
-		Card q = new Card(4, SeedType.CUORI);
-		Card u = new Card(1, SeedType.CUORI);
-	//	onTable.add(due);
-		onTable.add(t);
-		onTable.add(dieci);
-		onTable.add(o);
-		onTable.add(q);
-		onTable.add(u);
-		ArrayList<ArrayList<Card>> cards = GameProcessor.searchHandle(onTable, c);
-		System.out.println(cards);
 	}
 
 }
