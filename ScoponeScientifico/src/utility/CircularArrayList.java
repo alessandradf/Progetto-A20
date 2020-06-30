95cc4af1b0213b7633043acb616506f3973c3c1b
Andrea <30377977+TreCani@users.noreply.github.com>
2020-07-01 00:06:58 +0200

    Rimosso metodo inutile

--- ScoponeScientifico/src/utility/CircularArrayList.java
+++ ScoponeScientifico/src/utility/CircularArrayList.java
@@ -1,24 +1,10 @@
 package utility;
 
 import java.util.ArrayList;
-import model.Game;
 
 public class CircularArrayList<E> extends ArrayList<E> {
-	private static int CURRENT_INDEX = 0;
 
-	public E unlockNext() {
-		CURRENT_INDEX++;
-		if (CURRENT_INDEX == Game.PLAYER_NUMBER)	//Game.PLAYER_NUMBER = 4;
-			setCURRENT_INDEX(0);
-		return get(CURRENT_INDEX);
-	}
-	
 	public E unlockNext(int t) {
 		return get(t % size());
-		//Caro prof. Lombardi, le voglio un mondo di bene
-	}
-	
-	private static void setCURRENT_INDEX(int newIndex) {
-		CURRENT_INDEX = newIndex;
 	}
 }
